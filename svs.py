157e6974f997d62af22c77cb1efa2c46cc7c35a4
crozix <10584311+Crozix@users.noreply.github.com>
2019-08-02 02:05:35 +0200

    Made action case insensitive

--- svs.py
+++ svs.py
@@ -14,14 +14,14 @@ def main_disable(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("action", choices = ["Enable", "Disable"])
+    parser.add_argument("action", help="[Enable, Disable]")
     parser.add_argument("service", help="Service to Enable/Disable")
     parser.add_argument("-v", "--verbose", type = int, help = "Increase output verbosity")
     args = parser.parse_args()
 
-    if args.action == "Enable":
+    if f"{args.action}".lower() == "enable":
         main_enable(args)
-    elif args.action == "Disable":
+    elif f"{args.action}".lower() == "disable":
         main_disable(args)
 
 
