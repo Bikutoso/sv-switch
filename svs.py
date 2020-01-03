e4355a9f72cc77854427bb11cf75654ee6eb0c7d
Bikutoso <10584311+Bikutoso@users.noreply.github.com>
2020-01-03 15:18:26 +0100

    Lists services now displays disabled services instead of all avaiable services

--- svs.py
+++ svs.py
@@ -13,9 +13,9 @@ class Sv_Switch:
     PATH_SERVICE = "/tmp/svs/service/"
 
     def __init__(self):
-
         self.services_available = self._get_services(self.PATH_SV)
         self.services_enabled = self._get_services(self.PATH_SERVICE)
+        self.services_disabled = list(set(self.services_available) - set(self.services_enabled))
 
 
     def _get_services(self, path):
@@ -27,6 +27,7 @@ class Sv_Switch:
         return services
 
     def enable_service(self, args):
+        """Creates symlink for selected service"""
         if args.service not in self.services_enabled and args.service in self.services_available:
             os.symlink(self.PATH_SV + args.service, self.PATH_SERVICE + args.service, True)
             logging.debug(f"{args.service} enabled.")
@@ -36,6 +37,7 @@ class Sv_Switch:
 
 
     def disable_service(self, args):
+        """Removes symlink on selected service"""
         if args.service in self.services_enabled:
             os.unlink(self.PATH_SERVICE + args.service)
             logging.debug(f"{args.service} disabled.")
@@ -44,8 +46,8 @@ class Sv_Switch:
             logging.warning(f"Service {args.service} already Disabled")
 
     def list_services(self, args):
-        print(f"Enabled:   {sorted(self.services_enabled)}")
-        print(f"Available: {sorted(self.services_available)}")
+        print(f"Enabled:  {sorted(self.services_enabled)}")
+        print(f"Disabled: {sorted(self.services_disabled)}")
 
 def main():
     svs = Sv_Switch()
